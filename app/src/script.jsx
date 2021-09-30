4004ef48252945d8c9df4f15050357c97b708843
David Whitney <david@davidwhitney.co.uk>
2021-09-30 09:50:08 +0100

    Added react refresh

--- app/src/script.jsx
+++ app/src/script.jsx
@@ -9,3 +9,9 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root'),
 );
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+if (undefined /* [snowpack] import.meta.hot */) {
+    undefined /* [snowpack] import.meta.hot */.accept();
+}
